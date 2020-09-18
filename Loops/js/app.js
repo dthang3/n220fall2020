33103ab7780a8b218e969f1651697ba69c2502db
Daniel C Thang <danthang@iu.edu>
2020-09-18 16:33:21 -0400

    loopschanges

--- Loops/js/app.js
+++ Loops/js/app.js
@@ -1,27 +1,31 @@
 function setup () {
-    createCanvas (1350, 120);   
+    createCanvas (1500, 1500);   
 }
 function draw () {
-    background(500);
-    var y = 60;
+    var y = 50;
     //25 black circles
-    for (var x = 50; x < 1300; x += 50) {
-        fill(0);
-        circle (x, y, 50);
-        //devided by 3 and draw a purple circle
-        if (x / 3 == 0) {
-            fill(136, 3, 252);
-            circle(x, y, 50);
+    for (var i = 0; i < 25; ++i) {
+        var x = i*60;
+
+        if (i % 3 === 0 && i % 5 === 0){
+        //devidied by 3 and draw blue circle 
+            fill (0, 46, 250);
+            square(x,y,60);
         }
         // devided by 5 and draw green square
-        if (x / 5 == 0) {
-            fill(106, 191, 99);
-            square(x + 50, y + 50, 55);
+        else if ( i % 5 === 0) {
+            fill (56, 135, 0);
+            square (x, y, 50);
         }
         //Devided by 3 and 5, blue square
-        if (x / 3 == 0 && x / 5 == 0) {
-            fill(72, 95, 168);
-            square (x, y, 55);
+        else if (i % 3 === 0 ){
+            fill (144, 3, 252);
+            circle (x+35, y+35, 40);
+        }
+        //other draw black circle 
+        else {
+            fill (0)
+            circle (x+35, y+35, 40);
         }
     }    
 }
