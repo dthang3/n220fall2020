838d7885ec9c65772e0c20ec2be063f52e5e0e29
Daniel C Thang <danthang@iu.edu>
2020-11-06 16:43:17 -0500

    manipulation

---
+++ Objectmanipulation/personalcomposition/js/app.js
@@ -0,0 +1,10 @@
+let dvBox = document.getElementById("box");
+dvBox.innerHTML = "Click to increase Size";
+
+function myFunction(){
+   
+    dvBox.style.backgroundColor="#66fc03";
+    dvBox.style.height= "500px";
+    dvBox.style.width = "500px";
+
+}
